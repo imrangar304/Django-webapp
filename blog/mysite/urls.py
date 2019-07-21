27e1bf8fff4820528510890a5d7c6eb40b274399
imrangar304 <shubhamrangar786@gmail.com>
2019-07-22 00:15:18 +0530

    comments added to the article

--- blog/mysite/urls.py
+++ blog/mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.auth import views
+from django.contrib.auth import authenticate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-      path('', include('blog.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+     path('', include('blog.urls')),
 ]
